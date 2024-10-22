d4e4cca8327e0843812787decb299a10422bebcd
Shahine Benthanane <shahine93300@gmail.com>
2024-10-22 10:53:57 +0200

    Spawn Character

--- Source/CouchGame/Public/CouchGameMode.h
+++ Source/CouchGame/Public/CouchGameMode.h
@@ -6,6 +6,8 @@
 #include "GameFramework/GameModeBase.h"
 #include "CouchGameMode.generated.h"
 
+class ACouchCharacter;
+class APlayerStart;
 /**
  * 
  */
@@ -13,4 +15,31 @@ UCLASS()
 class COUCHGAME_API ACouchGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
+
+public:
+	virtual void BeginPlay() override;
+
+protected:
+	UPROPERTY()
+	TArray<ACouchCharacter*> CharactersInGame;
+	
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<class ACouchCharacter> CouchCharacterP0;
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<class ACouchCharacter> CouchCharacterP1;
+	
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<class ACouchCharacter> CouchCharacterP2;
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<class ACouchCharacter> CouchCharacterP3;
+
+private:
+	void FindPlayerStartActorsInArena(TArray<APlayerStart*>& ResultsActors);
+
+	void SpawnCharacter(const TArray<APlayerStart*>& SpawnPoints);
+
+	TSubclassOf<ACouchCharacter> GetCouchCharacterClassFromInputType(EAutoReceiveInput::Type InputType) const;
+	
 };
