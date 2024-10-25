4561defcb272f0ddd7f57eed922c7ef879a60e75
Venizio <gabinpincon@gmail.com>
2024-10-25 10:22:27 +0200

    [Test 2]

--- Source/LocalMultiplayer/Public/LocalMultiplayer.h
+++ Source/LocalMultiplayer/Public/LocalMultiplayer.h
@@ -6,6 +6,9 @@
 class FLocalMultiplayerModule : public IModuleInterface
 {
 public:
-    virtual void StartupModule() override;
-    virtual void ShutdownModule() override;
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+
+	UPROPERTY()
+	int a = 0;
 };
